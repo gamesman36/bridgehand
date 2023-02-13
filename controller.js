ed3524390b4aa05f4324818a56a03dfbd9f84eaf
gamesman36 <gamesman36@outlook.com>
2023-02-13 13:40:18 +0100

    Cleaning up sorting and counting code

--- controller.js
+++ controller.js
@@ -32,20 +32,24 @@ function whatSuit(){
     for(let x of dealtCards){
         if(x>1 && x<15){
             clubs.push(x);
-            highLowSort(clubs);
         }
+
         else if(x>14 && x<28){
             diamonds.push(x);
-            highLowSort(diamonds);
         }
+        
         else if(x>27 && x<41){
             hearts.push(x);
-            highLowSort(hearts);
         }
+
         else {
             spades.push(x);
-            highLowSort(spades);
         }
+
+        highLowSort(clubs);
+        highLowSort(diamonds);
+        highLowSort(hearts);
+        highLowSort(spades);
     } 
 }
 
@@ -55,10 +59,10 @@ function highLowSort(arr){
 
 function countHighCardPts(){
     for(let x of dealtCards){
-        if(x === 14 || x === 27 || x === 40 || x == 53) points += 4;
-        if(x === 13 || x === 26 || x === 39 || x == 52) points += 3;
-        if(x === 12 || x === 25 || x === 38 || x == 51) points += 2;
-        if(x === 11 || x === 24 || x === 37 || x == 50) points += 1;     
+        if(x === 14 || x === 27 || x === 40 || x === 53) points += 4;
+        if(x === 13 || x === 26 || x === 39 || x === 52) points += 3;
+        if(x === 12 || x === 25 || x === 38 || x === 51) points += 2;
+        if(x === 11 || x === 24 || x === 37 || x === 50) points += 1;     
     }
 }
 
